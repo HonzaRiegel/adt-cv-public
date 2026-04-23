7072e50811cb011800da726b96eefdeb8ea77519
Miloslav Konopik <konopik@kiv.zcu.cz>
2026-04-23 12:15:26 +0200

    Vyřešené cvičení 6, 7, 8

--- 08-knapsack/fib.py
+++ 08-knapsack/fib.py
@@ -6,6 +6,7 @@ from utils import measure_time
 def fib(n: int) -> int:
     if n <= 1:
         return n
+
     return fib(n - 1) + fib(n - 2)
 
 @functools.cache
@@ -23,17 +24,32 @@ def fib_mem(n: int, lookup: dict[int, int]) -> int:
 
     return lookup[n]
 
+  
+
+def fib_iter2(n: int) -> int:
+    if n <= 1:
+        return n
+
+    a, b = 0, 1
+    
+    for _ in range(2, n + 1):
+        a, b = b, a + b
+        
+    return b
 
 def main() -> None:
     lookup: dict[int, int] = {}
 
-    a = 20 # to je hned
+    a = 200 # to je hned
     # a = 30 # to už chvilku trvá
     # a = 40 # za jak dlouho se asi dočkáme?
 
     measure_time(lambda: fib_cache(a), 100)
-    measure_time(lambda: fib_mem(a, lookup), 100)
-    measure_time(lambda: fib(a))
+    measure_time(lambda: fib_mem(a, {}), 100)
+    measure_time(lambda: fib_iter2(a), 100)
+    #measure_time(lambda: fib(a))
+    
+    print(fib_cache(a))
 
 
 if __name__ == "__main__":
